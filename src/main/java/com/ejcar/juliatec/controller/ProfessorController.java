d646ace5894131786ff6a7da5113407c2671a647
5456ju00 <julia.caroline.goncalves@escola.pr.gov.br>
2025-06-27 12:20:25 -0300

    juntando back-en com front-end

---
+++ src/main/java/com/ejcar/juliatec/controller/ProfessorController.java
@@ -0,0 +1,19 @@
+package com.ejcar.juliatec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/professor")
+public class ProfessorController {
+    
+
+    @GetMapping( value = "/")
+    public void imprimir(){
+        System.out.println("chamou o metodo imprimir");
+    }
+
+
+    
+}
