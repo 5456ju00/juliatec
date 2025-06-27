bda2d83d5700dd8433938c7637aed822c1e04eb9
5456ju00 <julia.caroline.goncalves@escola.pr.gov.br>
2025-06-27 11:24:56 -0300

    commit

---
+++ src/main/java/com/ejcar/juliatec/JuliatecApplication.java
@@ -0,0 +1,13 @@
+package com.ejcar.juliatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuliatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuliatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejcar/juliatec/JuliatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejcar.juliatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuliatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
