ee5a75b29d32d31b7762c08cc19f2d2bb229016d
5456ju00 <julia.caroline.goncalves@escola.pr.gov.br>
2025-07-08 10:41:15 -0300

    implementando o método CRUD

--- src/main/java/com/ejcar/juliatec/controller/ProfessorController.java
+++ src/main/java/com/ejcar/juliatec/controller/ProfessorController.java
@@ -5,7 +5,9 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,25 @@ public class ProfessorController {
         return professorRepository.findAll();
     }
 
+    @DeleteMapping( value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        System.out.println( "O valor do id -->" + id);
+        professorRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @GetMapping( value = "/{id}")
+    public ResponseEntity<Professor> select(@PathVariable Long id) {
+
+        System.out.println( "O valor do id -->" + id);
+        return ResponseEntity.ok().body(professorRepository.findById(id).orElse(null));
+
+    }
+
+
+
     @PostMapping( value = "/insert")
     public ResponseEntity<?> insert(@RequestBody ProfessorDto professorDto) {
 
@@ -61,3 +82,55 @@ public class ProfessorController {
     }
     
 }
+
+
+
+
+
+
+
+
+//******************************************************************************************************************************* */
+                                        //Criar o esqueleto do método.  
+                                        //Como receber a informação do que eu preciso?
+                                        //    public String nomeDoMetodo(entrada) {
+                                                    //sem saída
+                                        //        return // => Saída do método
+
+                                        //    }
+
+                                        //Preparar o método para receber a entrada
+                                        //  public void delete ( Long id ) {
+                                        //}
+
+
+                                        //O que eu preciso fazer?
+                                        //Preciso apagar (deletar) um registro.
+                                        //Preciso de alguma informação para completar a tarefa?
+                                        
+                                        //Já tenho o id (imformação)
+                                        //Preciso ultilizar o Repository (Banco de Dados)
+                                        //Escolher um dos métodos do CRUD
+
+                                        //C => CREATE => INSERT
+                                        //R => READ => SELECT
+                                        //U => UPDATE => UPDATE
+                                        //D => DELETE => DELETE
+
+                                        //Preciso Mappear o método, afim de chamar da camada externa 
+                                        //Como injetar o valor do id na entrada do método de forma dinâmica, pois não posso mappear para todos os ids
+                                        //EXAMPLO:
+                                        // /delete/1
+                                        // /delete/2
+                                        // /delete/3 
+                                        //e DeleteMapping( value = "/delete")
+                                        
+                                        //Criar Resposta (Response) mais Adequada
+                                            //como foi efetuado o delete de um recurso
+                                            //Se eu quiser posso devolver uma resposta ou não 
+                                        // => Resposta Padrão
+                                            // public ResponseEntity<Void> delete(@PathVariable Long id){
+
+                                            //}
+                           
+
